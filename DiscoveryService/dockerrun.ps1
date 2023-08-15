23238eaae63357a7abe125940225cc5252a6d6c8
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2023-08-15 22:13:19 +0200

    Add docker network to dockerrun.ps1 scripts

--- DiscoveryService/dockerrun.ps1
+++ DiscoveryService/dockerrun.ps1
@@ -1,1 +1,5 @@
+if (-not (docker network ls -q -f "name=fbaa-network")) {
+    docker network create fbaa-network
+}
+
 docker run -e FBAA_ADDRESS=localhost -e FBAA_PORT=8080 --rm fbaa-discovery
--- HQServer/HQServer/dockerrun.ps1
+++ HQServer/HQServer/dockerrun.ps1
@@ -1,1 +1,5 @@
+if (-not (docker network ls -q -f "name=fbaa-network")) {
+    docker network create fbaa-network
+}
+
 docker run -p 8080:80 --rm fbaa-server
