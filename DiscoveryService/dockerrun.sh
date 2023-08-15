7be24cefd774977d12d1ef7b4c1a2e36a563dcaf
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2023-08-15 22:16:40 +0200

    Fix interval in dockerrun.sh to 60 seconds

--- DiscoveryService/dockerrun.sh
+++ DiscoveryService/dockerrun.sh
@@ -4,4 +4,4 @@ if [[ ! $(docker network ls -q -f name=fbaa-network) ]]; then
     docker network create fbaa-network
 fi
 
-docker run --network fbaa-network -e FBAA_ADDRESS=fbaa-server -e FBAA_PORT=8080 -e FBAA_INTERVAL=5 --name fbaa-discovery --rm fbaa-discovery
+docker run --network fbaa-network -e FBAA_ADDRESS=fbaa-server -e FBAA_PORT=8080 -e FBAA_INTERVAL=60 --name fbaa-discovery --rm fbaa-discovery
