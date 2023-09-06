a003b63490f4fe3ac918d8f515998cab529509c3
Dewald Swanepoel <dewald.swanepoel@gmail.com>
2023-09-06 12:30:50 +0200

    Fix dockerrun scripts for Discovery Service

--- DiscoveryService/dockerrun.ps1
+++ DiscoveryService/dockerrun.ps1
@@ -2,4 +2,4 @@ if (-not (docker network ls -q -f "name=fbaa-network")) {
     docker network create fbaa-network
 }
 
-docker run -e FBAA_ADDRESS=localhost -e FBAA_PORT=8080 -e FBAA_INTERVAL=60 --rm fbaa-discovery
+docker run -e FBAA_ADDRESS=localhost -e FBAA_PORT=80 -e FBAA_INTERVAL=60 --rm fbaa-discovery
